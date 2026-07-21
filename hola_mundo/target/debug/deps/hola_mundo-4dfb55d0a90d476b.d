05914317cb9088232bc6340c0e567b0ecc6ee7c6
RafxDev <jrafaelmorantess@gmail.com>
2026-07-21 15:27:31 -0500

    correcion

---
+++ hola_mundo/target/debug/deps/hola_mundo-4dfb55d0a90d476b.d
@@ -0,0 +1,5 @@
+C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\hola_mundo-4dfb55d0a90d476b.d: src\main.rs
+
+C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\libhola_mundo-4dfb55d0a90d476b.rmeta: src\main.rs
+
+src\main.rs:
