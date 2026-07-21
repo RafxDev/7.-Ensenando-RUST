3c00f65ce95028f6f77000d415a20fdb371fc06d
RafxDev <jrafaelmorantess@gmail.com>
2026-07-21 15:29:10 -0500

    Primer Hola Mundo

--- hola_mundo/target/debug/deps/hola_mundo-28c2d899a010ca94.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\hola_mundo-28c2d899a010ca94.d: src\main.rs
-
-C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\libhola_mundo-28c2d899a010ca94.rmeta: src\main.rs
-
-src\main.rs:
--- hola_mundo/target/debug/deps/hola_mundo-4dfb55d0a90d476b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\hola_mundo-4dfb55d0a90d476b.d: src\main.rs
-
-C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\libhola_mundo-4dfb55d0a90d476b.rmeta: src\main.rs
-
-src\main.rs:
