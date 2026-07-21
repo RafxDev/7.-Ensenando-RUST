0eae454fefe6f144d6c3e884451b4a499810d48d
RafxDev <jrafaelmorantess@gmail.com>
2026-07-21 15:24:08 -0500

    Primer hola mundo

---
+++ hola_mundo/target/debug/deps/hola_mundo-236121f47dadb538.d
@@ -0,0 +1,5 @@
+C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\hola_mundo-236121f47dadb538.d: src\main.rs
+
+C:\Users\jrafa\Documents\Programacion\7. Ensenando RUST\hola_mundo\target\debug\deps\hola_mundo-236121f47dadb538.exe: src\main.rs
+
+src\main.rs:
---
+++ hola_mundo/target/debug/hola_mundo.d
@@ -0,0 +1,1 @@
+C:\Users\jrafa\Documents\Programacion\7.\ Ensenando\ RUST\hola_mundo\target\debug\hola_mundo.exe: C:\Users\jrafa\Documents\Programacion\7.\ Ensenando\ RUST\hola_mundo\src\main.rs
